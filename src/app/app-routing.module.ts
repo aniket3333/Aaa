ac26964dffd3865d299f3ca67803b131b6a3cd19
aniket3333 <anikethiware3@gmail.com>
2024-07-22 18:57:26 +0530

    add login component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmpLoginComponent } from './component/login/emp-login/emp-login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+path:'',
+redirectTo:'login',
+pathMatch:'full'
+  },
+  {
+path:'login',
+component:EmpLoginComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/component/login/emp-login/emp-login.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-emp-login',
+  templateUrl: './emp-login.component.html',
+  styleUrls: ['./emp-login.component.scss']
+})
+export class EmpLoginComponent {
+
+}
